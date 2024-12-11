b16093267944b74ecd79937d3b192bea2f1d224e
miguelamaral254 <miguel.amaral.sales@gmail.com>
2024-12-11 13:22:29 -0300

    adcionado .env

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,15 +1,17 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const config = {
-    HOST: "localhost",
-    PORT: 3306,
-    USER: "root", // seu user do mysql
-    PASSWORD: "root", //sua senha do mysql
-    DB: "AppEvento", // nome do schemamudar se preciso
+    HOST: process.env.DB_HOST,
+    PORT: parseInt(process.env.DB_PORT as string, 10),
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+        max: parseInt(process.env.DB_POOL_MAX as string, 10),
+        min: parseInt(process.env.DB_POOL_MIN as string, 10),
+        acquire: parseInt(process.env.DB_POOL_ACQUIRE as string, 10),
+        idle: parseInt(process.env.DB_POOL_IDLE as string, 10)
     }
 };
-
-export const dialect = "mysql";
--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,18 +1,16 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { config, dialect } from "../config/db.config"
-import { Categoria } from "../models/categoria"
-// import { Cliente } from "../models/cliente" importar a model depois de pronta
-
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { config } from "../config/db.config"; 
+import { Categoria } from "../models/categoria";
 
 export const AppDataSource = new DataSource({
-    type: dialect,
-    host: config.HOST,
+    type: config.dialect as "mysql" | "postgres" | "sqlite" | "mssql" | "oracle",
+    host: config.HOST!,
     port: config.PORT,
-    username: config.USER,
-    password: config.PASSWORD,
-    database: config.DB,
-    entities: [Categoria, /*Cliente*/], // importar a entidade/model aqui
-    synchronize: false, // ligar quando os modelos estiverem prontos para criar as tabelas no banco de dados
+    username: config.USER!,
+    password: config.PASSWORD!,
+    database: config.DB!,
+    entities: [Categoria],
+    synchronize: true, 
     logging: false,
-})
+});
